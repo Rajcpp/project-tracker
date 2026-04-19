81fb8e560da6f4ad1e9ed5d928b98b95b2793b4b
Rajmaurya2901 <raj.maurya.29.01.08>
2026-04-19 19:11:48 +0530

    added logout button logic

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -73,3 +73,8 @@ task_list.addEventListener("click", async (event) => {
     }
   }
 });
+
+document.getElementById("logout-btn").addEventListener("click", () => {
+  localStorage.removeItem("token");
+  window.location.href = "/";
+});
